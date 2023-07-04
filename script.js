f3b4f717e7e7072d8c93208e73985510af925b66
Robin Singh <127648531+robinsingh987@users.noreply.github.com>
2023-07-04 07:10:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function navigation(){
+    var icon=document.getElementById("icon");
+
+    var navbar=document.getElementById("navbar");
+
+    if(icon.classList.contains('fa-bars')){
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+        navbar.style.left = '0';
+    }
+    else{
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+        navbar.style.left = '-100%';
+    }
+}
